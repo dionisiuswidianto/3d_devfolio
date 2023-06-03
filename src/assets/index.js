29651da9ce7209c8ac2aa2348ea7a3bfea8369f7
dionisiuswidianto <64462297+dionisiuswidianto@users.noreply.github.com>
2023-06-03 20:34:07 +0700

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from './logo.png';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+// import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import figma from './tech/figma.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import reactjs from './tech/reactjs.png';
+import tailwind from './tech/tailwind.png';
+import threejs from './tech/threejs.svg';
+import sql from './tech/sql.svg';
+import axure from './tech/axure.png';
+import php from './tech/php.svg';
+import coldFusion from './tech/coldfusion.png';
+
+import vegas from './tech/vegas.png';
+import umn from './company/umn.png';
+import jala from './company/jala.png';
+import freelance from './company/freelance.png';
+import pasas from './company/pasas.png';
+
+import nodejs from './tech/nodejs.png';
+import koperasimobile from './KoperasiMobile.png';
+import salontablet from './SalonTablet.png';
+import inventoryweb from './InventoryWeb.png';
+
+import heri from './testimonials/heri.jpg';
+import andreas from './testimonials/andreas.jpg';
+import iqi from './testimonials/iqi.jpg';
+
+export {
+  koperasimobile,
+  salontablet,
+  inventoryweb,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  menu,
+  close,
+  css,
+  figma,
+  html,
+  javascript,
+  reactjs,
+  tailwind,
+  nodejs,
+  vegas,
+  threejs,
+  sql,
+  axure,
+  php,
+  coldFusion,
+  umn,
+  jala,
+  freelance,
+  pasas,
+  heri,
+  andreas,
+  iqi,
+};
